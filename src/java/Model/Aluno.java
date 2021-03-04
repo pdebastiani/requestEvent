cdcb4893bf348abc56ca0b32007e705eae974dfe
Paulo Sergio Debastiani <paulo.s.debastiani@gmail.com>
2021-03-04 20:39:47 -0300

    primeiro commit Paulo S Debastiani

--- src/java/Model/Aluno.java
+++ src/java/Model/Aluno.java
@@ -124,10 +124,14 @@ public class Aluno {
     public ResultSet consultarInner() {
         Connection con = Conexao.conectar();
         String sql = "select a.id, a.nome || ' ' || a.sobrenome nomecompleto, " +
-                "s.sala etapa1, a.idsala2 etapa2 " +
-                "from aluno a, sala s " +
-                "where a.idSala1 = s.id " +
-                "order by a.id;";
+                "(select s1.sala " +
+                "from sala s1 " +
+                "where a.idSala1 = s1.id) etapa1, " +
+                "(select s2.sala " +
+                "from sala s2 " +
+                "where a.idSala2 = s2.id) etapa2 " +
+                "from aluno a " +
+                "order by a.nome;";
         ResultSet rs = null;
         try {
             PreparedStatement stm = con.prepareStatement(sql);
